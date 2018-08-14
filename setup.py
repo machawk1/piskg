3054db438c7319fca1655aad74d4b007b34d4b8e
Mat Kelly <me@matkelly.com>
2018-08-14 11:08:17 -0400

    Add missing comma

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7"
+        "Programming Language :: Python :: 3.7",
         
         "Topic :: Security :: Cryptography",
         
