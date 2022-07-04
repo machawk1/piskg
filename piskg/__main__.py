3ceb61503901975c13c9e8884a250ac62f484bb7
julanteria <j.vorpahl@live.de>
2022-07-04 12:11:39 +0200

    fixed b'<key>' to <key>

--- piskg/__main__.py
+++ piskg/__main__.py
@@ -7,7 +7,7 @@ def main():
     key = b2a_hex(urandom(32))
     print('/key/swarm/psk/1.0.0/')
     print('/base16/')
-    #key = b'<key>', so we need to strip: b''
+    #key = b'<key>', so we need to strip: b''.
     key = str(key)
     print(key[2:-1])
 
